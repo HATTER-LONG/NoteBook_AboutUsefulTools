5ff6c3fc87c40dddfc044b12ab1d27f5353a8872
CaoLei <caolei6767@gmail.com>
2021-01-31 00:07:52 +0800

    计时器工具提交

---
+++ 常用代码片段/RAII记时器/TestTimer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+namespace ToolBox
+{
+struct TestTimer
+{
+    TestTimer(const std::string& text);
+    ~TestTimer();
+
+    std::chrono::time_point<std::chrono::system_clock> Start;
+    std::chrono::time_point<std::chrono::system_clock> Stop;
+    std::chrono::microseconds Elapsed;
+    std::string Text;
+};
+}   // namespace ToolBox
