1b43b1d2ded96541bcda59cbc8578e8fb7f976e3
Layton <caolei6767@gmail.com>
2021-08-22 00:14:55 +0800

    修正安装脚本

--- LinuxTerminalConfig/install.sh
+++ LinuxTerminalConfig/install.sh
@@ -3,7 +3,7 @@
 # 更新子模块
 git submodule sync
 git submodule update --init --recursive
-git submodule foreach git pull
+git submodule update --remote --merge
 
 ZSH=~/.oh-my-zsh
 ZSHRC=~/.zshrc
@@ -15,13 +15,13 @@ echo $CURRENT
 echo $ZSH
 
 echo "安装 vim config .........."
-ln -s ./vim_runtime $VIMRUNTIME
-cd VIMRUNTIME
-install_awesome_vimrc.sh
+ln -s $CURRENT/vim_runtime $VIMRUNTIME
+cd $VIMRUNTIME
+./install_awesome_vimrc.sh
 cd -
 
 echo "安装 oh my zsh ..........."
-ln -s $CURRENT/ohmyzsh_plugins/* ./ohmyzsh/custom/plugins/
+ln -s $CURRENT/ohmyzsh_plugins/* $CURRENT/ohmyzsh/custom/plugins/
 ln -s $CURRENT/ohmyzsh $ZSH
 ln -s $CURRENT/zshrc $ZSHRC
 
