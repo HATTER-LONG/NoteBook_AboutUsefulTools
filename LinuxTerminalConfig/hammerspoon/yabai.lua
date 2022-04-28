37b1a5c07e7f2887cbee29b64dc4e1d4743404cc
Layton <caolei6767@gmail.com>
2022-04-28 22:25:21 +0800

    add yabai call method

---
+++ LinuxTerminalConfig/hammerspoon/yabai.lua
@@ -0,0 +1,24 @@
+return function(args, completion)
+	local yabai_output = ""
+	local yabai_error = ""
+	-- Runs in background very fast
+	local yabai_task = hs.task.new("yabai", function(err, stdout, stderr)
+		print()
+	end, function(_, stdout, stderr) -- _ is task
+		print("stdout:" .. stdout, "stderr:" .. stderr)
+		if stdout ~= nil then
+			yabai_output = yabai_output .. stdout
+		end
+		if stderr ~= nil then
+			yabai_error = yabai_error .. stderr
+		end
+		return true
+	end, args)
+
+	if type(completion) == "function" then
+		yabai_task:setCallback(function()
+			completion(yabai_output, yabai_error)
+		end)
+	end
+	yabai_task:start()
+end
